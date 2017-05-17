6f1067ef2c7a000b54a30d5dbb6cd3bc03247b28
Stevie Howat <skhowat@gmail.com>
2017-05-17 11:39:18 -0500

    functional improvements -- setting classes via state, set initial gameboard on componentDidMount, improved newGame function

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -4,31 +4,48 @@ import Incorrect from './Incorrect.js';
 import Correct from './Correct.js';
 import Solutions from './Solutions.js';
 
-const currentAnswer = Solutions[Math.floor(Math.random() * Solutions.length)];
-
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+      currentAnswer: '',
+      gameboard: [],
+      guesses: 6,
+      picIndex: 0,
+      message: 'Click a letter to make a guess',
+      rightGuess: [],
+      wrongGuess: []
+    }
+  }
+
+  componentDidMount() {
+    const currentAnswer = Solutions[Math.floor(Math.random() * Solutions.length)];
+    this.setState({
+      currentAnswer: currentAnswer,
+      gameboard: currentAnswer.answer.replace(/[\w]/g, '_').split("")
+    });
+  }
+
+  newGame() {
+    const currentAnswer = Solutions[Math.floor(Math.random() * Solutions.length)];
+    this.setState({
+      letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
       currentAnswer: currentAnswer,
       gameboard: currentAnswer.answer.replace(/[\w]/g, '_').split(""),
       guesses: 6,
       picIndex: 0,
-      message: 'Click a letter to make a guess'
-    }
-  }
-
-  newGame() {
-    window.location.reload();
+      message: 'Click a letter to make a guess',
+      rightGuess: [],
+      wrongGuess: []
+    });
   }
 
   answerClick(evt) {
     if (this.state.guesses === 0)
       { return; }
-    if (evt.target.className === "answers right" || evt.target.className === "answers wrong")
-      { return; }
+    if (this.state.rightGuess.indexOf(evt.target.id) > -1 || this.state.wrongGuess.indexOf(evt.target.id) > -1)
+        { return; }
     else if (this.state.currentAnswer.answer.indexOf(evt.target.id) > -1) {
       let newboard = this.state.gameboard;
       for (let i=0; i<this.state.currentAnswer.answer.length; i++) {
@@ -36,17 +53,20 @@ class App extends Component {
           newboard[i] = evt.target.id;
         }
       }
-      this.setState({gameboard: newboard, message: 'Correct!'});
-      evt.target.className = "answers right";
+      let newRight = this.state.rightGuess.slice();
+      newRight.push(evt.target.id);
+      this.setState({gameboard: newboard, message: 'Correct!', rightGuess: newRight});
       this.checkWin();
     }
     else if (this.state.guesses === 1) {
-      this.setState({ guesses: this.state.guesses-1, picIndex: this.state.picIndex+1, message: 'No more guesses. Click "New Game" to play again!'});
-      evt.target.className = "answers wrong";
+      let newWrong = this.state.wrongGuess.slice();
+      newWrong.push(evt.target.id);
+      this.setState({ guesses: this.state.guesses-1, picIndex: this.state.picIndex+1, message: 'No more guesses. Click "New Game" to play again!', wrongGuess: newWrong});
     }
     else {
-      this.setState({ guesses: this.state.guesses-1, picIndex: this.state.picIndex+1, message: 'Try again!' });
-      evt.target.className = "answers wrong";
+      let newWrong = this.state.wrongGuess.slice();
+      newWrong.push(evt.target.id);
+      this.setState({ guesses: this.state.guesses-1, picIndex: this.state.picIndex+1, message: 'Try again!', wrongGuess: newWrong });
     }
   }
 
@@ -66,7 +86,18 @@ class App extends Component {
 
   render() {
 
-    let answers = this.state.letters.map((x) => <li className="answers" key={x} id={x} onClick={(evt) => this.answerClick(evt)}>{x}</li>)
+    let answers = this.state.letters.map((x) => {
+       if (this.state.rightGuess.indexOf(x) > -1) {
+         return <li className="answers right" key={x} id={x} onClick={(evt) => this.answerClick(evt)}>{x}</li>
+       }
+       else if (this.state.wrongGuess.indexOf(x) > -1) {
+         return <li className="answers wrong" key={x} id={x} onClick={(evt) => this.answerClick(evt)}>{x}</li>
+       }
+       else {
+         return <li className="answers" key={x} id={x} onClick={(evt) => this.answerClick(evt)}>{x}</li>
+       }
+     });
+
 
     return (
       <div className="App">
