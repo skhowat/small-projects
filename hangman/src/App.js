f6d7ee707713eb0383111913c2805f78700d2522
Stevie Howat <skhowat@gmail.com>
2017-05-17 16:30:52 -0500

    added comments, updated letter list styling

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -18,8 +18,9 @@ class App extends Component {
       wrongGuess: []
     }
   }
-  
+
   componentDidMount() {
+    // Sets the state with a new gameboard and solution.
     const currentAnswer = Solutions[Math.floor(Math.random() * Solutions.length)];
     this.setState({
       currentAnswer: currentAnswer,
@@ -28,6 +29,7 @@ class App extends Component {
   }
 
   newGame() {
+    // Resets the state with a new gameboard and solution.
     const currentAnswer = Solutions[Math.floor(Math.random() * Solutions.length)];
     this.setState({
       letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
@@ -42,41 +44,56 @@ class App extends Component {
   }
 
   answerClick(evt) {
+    // If player has no guesses left, take no action.
     if (this.state.guesses === 0)
       { return; }
+    // If the selected letter has been previously selected, take no action.
     if (this.state.rightGuess.indexOf(evt.target.id) > -1 || this.state.wrongGuess.indexOf(evt.target.id) > -1)
         { return; }
+    // Check to see if the selected letter is included in the solution.
     else if (this.state.currentAnswer.answer.indexOf(evt.target.id) > -1) {
+    // Copy the current gameboard.
       let newboard = this.state.gameboard;
+    // Looping through the correct solution, if the letter is found at the current index, add it to the copied gameboard at the corresponding index.
       for (let i=0; i<this.state.currentAnswer.answer.length; i++) {
         if (this.state.currentAnswer.answer[i] === evt.target.id) {
           newboard[i] = evt.target.id;
         }
       }
+      // Copy the rightGuess array from the current state and add the selected letter to it.
       let newRight = this.state.rightGuess.slice();
       newRight.push(evt.target.id);
+      // Set the state with the new gameboard, message, and rightGuess array.
       this.setState({gameboard: newboard, message: 'Correct!', rightGuess: newRight});
+      // Check to see if the player has won the game.
       this.checkWin();
     }
+    // If the selected letter is not included in the solution, check to see if this is the last available guess.
     else if (this.state.guesses === 1) {
+      // Copy the wrongGuess array from the current state and add the selected letter to it.
       let newWrong = this.state.wrongGuess.slice();
       newWrong.push(evt.target.id);
+      // Set the state: decrement the guesses counter, increment the picIndex counter, update the message, and update the wrongGuess array.
       this.setState({ guesses: this.state.guesses-1, picIndex: this.state.picIndex+1, message: 'No more guesses. Click "New Game" to play again!', wrongGuess: newWrong});
     }
     else {
+      // The selected letter is not included in the solution, and the game is not yet over. Copy the wrongGuess array from the current state and add the selected letter to it.
       let newWrong = this.state.wrongGuess.slice();
       newWrong.push(evt.target.id);
+      // Set the state with updated wrongGuess array, decrement guess counter, increment picIndex counter, update the message.
       this.setState({ guesses: this.state.guesses-1, picIndex: this.state.picIndex+1, message: 'Try again!', wrongGuess: newWrong });
     }
   }
 
   checkWin() {
+    // If there are no blanks left on the gameboard, update the message and set the guess counter to 0 to end the game.
     if (this.state.gameboard.indexOf('_') === -1) {
       this.setState({message: 'You win!', guesses: 0});
     }
   }
 
   revealAnswer() {
+    // Set the current gameboard to the current solution, update the message, and set the guess counter to 0 to end the game.
     this.setState({
       gameboard: this.state.currentAnswer.answer,
       message: 'Click "New Game" to play again!',
@@ -85,11 +102,13 @@ class App extends Component {
   }
 
   render() {
-
+    // Map over array of letters to create list items.
     let answers = this.state.letters.map((x) => {
+      // If the letter is also included in the current rightGuess array in state, add the "right" className.
        if (this.state.rightGuess.indexOf(x) > -1) {
          return <li className="answers right" key={x} id={x} onClick={(evt) => this.answerClick(evt)}>{x}</li>
        }
+       // If the letter is also included in the current wrongGuess array in state, add the "wrong" className.
        else if (this.state.wrongGuess.indexOf(x) > -1) {
          return <li className="answers wrong" key={x} id={x} onClick={(evt) => this.answerClick(evt)}>{x}</li>
        }
