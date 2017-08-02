00ff82133ceba00f63cf8ee907a60a029c0015a6
Stevie Howat <skhowat@gmail.com>
2017-08-01 21:18:13 -0500

    shuffled answer options, clean up escaped characters from question

--- quizzer/src/Card.js
+++ quizzer/src/Card.js
@@ -6,15 +6,46 @@ class Card extends Component {
 
     console.log(this.props);
 
+    function shuffle(arr) {
+      let i = arr.length;
+      let temp;
+      let rand;
+
+      while(i) {
+        rand = Math.floor(Math.random() * i);
+        i -= 1;
+        temp = arr[i];
+        arr[i] = arr[rand];
+        arr[rand] = temp;
+      }
+
+      return arr;
+    }
+
     let answer = this.props.correct;
     let options = this.props.incorrect.slice();
     options.push(answer);
     console.log(options);
+    let shuffled = shuffle(options);
+    console.log('shuffled?', shuffled);
+
+    let choices = shuffled.map((x, i) => <li key={i}>{x}</li>);
+
+    let question = this.props.question;
+    question = question.replace(/&quot;/g, '"');
+    question = question.replace(/&#039;/g, "'");
+    question = question.replace(/&ldquo;/g, '"');
+    console.log(question);
+
+
 
     return (
       <div className="card">
         <div className="front">
-          {this.props.question}
+          {question}
+          <ul>
+            {choices}
+          </ul>
         </div>
         <div className="back">
           {this.props.correct}
